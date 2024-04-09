03afab0032ee7501c6710717eba07c80f012053c
Anthony Ward <pants1984@gmail.com>
2024-04-09 19:55:02 +0100

    Add code to retrieve contributors' statistics and write them to a CSV file

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Octokit;
 
@@ -7,20 +10,40 @@ class Program
     static async Task Main(string[] args)
     {
         var github = new GitHubClient(new ProductHeaderValue("MyAmazingApp"));
-        github.Credentials = new Credentials("your-github-token"); // NOTE: replace "your-github-token" with your actual token
+        var token = Environment.GetEnvironmentVariable("GH_TOKEN");
+        github.Credentials = new Credentials(token);
 
         var repositories = await github.Repository.GetAllForOrg("your-organisation"); // NOTE: replace "your-organisation" with your actual organisation name
 
+        var contributors = new Dictionary<string, int>();
+
         foreach (var repo in repositories)
         {
-            Console.WriteLine($"Repo: {repo.Name}");
-
             var stats = await github.Repository.Statistics.GetContributors(repo.Id);
 
             foreach (var stat in stats)
             {
-                Console.WriteLine($"User: {stat.Author.Login}, Contributions: {stat.Total}");
+                if (contributors.ContainsKey(stat.Author.Login))
+                {
+                    contributors[stat.Author.Login] += stat.Total;
+                }
+                else
+                {
+                    contributors[stat.Author.Login] = stat.Total;
+                }
             }
         }
+
+        using (var writer = new StreamWriter("output.csv"))
+        {
+            writer.WriteLine("Author,Contributions");
+
+            foreach (var contributor in contributors)
+            {
+                writer.WriteLine($"{contributor.Key},{contributor.Value}");
+            }
+        }
+
+        Console.WriteLine("Data written to output.csv");
     }
 }
